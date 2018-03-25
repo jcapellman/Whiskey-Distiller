268df360823de560eff12d01e0ad7faf3ff42697
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 13:47:15 -0500

    Added: Initial work on the GameManager class

---
+++ src/WhiskeyDistiller.library/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+
+namespace WhiskeyDistiller.library.Managers
+{
+    public class GameManager : BaseManager
+    {
+        private const double NEWGAME_INITIAL_CASH = 100;
+        private int NEWGAME_INITIAL_GAMEYEAR = DateTime.Now.Year;
+        private const int NEWGAME_INITAL_GAMEQUARTER = 1;
+
+        public Game CreateNewGame(string distilleryName)
+        {
+            var game = new Game
+            {
+                DistilleryName = distilleryName,
+                Cash = NEWGAME_INITIAL_CASH,
+                GameYear = NEWGAME_INITIAL_GAMEYEAR,
+                GameQuarter = NEWGAME_INITAL_GAMEQUARTER
+            };
+
+            IoC.DatabaseManager.Add(game);
+
+            return game;
+        }
+    }
+}
