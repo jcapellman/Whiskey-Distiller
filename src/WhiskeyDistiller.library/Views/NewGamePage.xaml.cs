fa4951290a38a349efaa2338703a5810fff7f6db
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 13:46:55 -0500

    Added: Initial NewGamePage

---
+++ src/WhiskeyDistiller.library/Views/NewGamePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewGamePage : ContentPage
+	{
+		public NewGamePage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new NewGameVM(Navigation);
+		}
+	}
+}
