f5a4b0f2a87194c7ba9249eafe9d1e785cb66047
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 11:15:01 -0500

    Added: Additional interface method definitions in the IDatabase class

--- src/WhiskeyDistiller.library/Interfaces/IDatabase.cs
+++ src/WhiskeyDistiller.library/Interfaces/IDatabase.cs
@@ -1,9 +1,15 @@
-﻿using WhiskeyDistiller.library.DAL.Tables;
+﻿using System;
+
+using WhiskeyDistiller.library.DAL.Tables;
 
 namespace WhiskeyDistiller.library.Interfaces
 {
     public interface IDatabase
     {
+        bool InitializeDB();
+
         bool Add<T>(T obj) where T : BaseTable;
+
+        bool CreateTable(Type tableType);
     }
 }
