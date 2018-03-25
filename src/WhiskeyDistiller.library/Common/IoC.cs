1321912aa1eb0640192c34e50d267ffba05e6d6a
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 11:11:05 -0500

    Added: IoC Class to handle the DI

---
+++ src/WhiskeyDistiller.library/Common/IoC.cs
@@ -0,0 +1,23 @@
+﻿using Ninject;
+
+using WhiskeyDistiller.library.Implementations;
+using WhiskeyDistiller.library.Managers;
+
+namespace WhiskeyDistiller.library.Common
+{
+    public static class IoC
+    {
+        private static IKernel Kernel { get; set; } = new StandardKernel();
+
+        public static DBManager Database => Kernel.Get<DBManager>();
+
+        public static void Setup()
+        {
+            var sqliteDatabase = new SQLiteDatabase();
+
+            sqliteDatabase.InitializeDB();
+
+            Kernel.Bind<DBManager>().ToSelf().InSingletonScope().WithConstructorArgument("database", sqliteDatabase);
+        }
+    }
+}
