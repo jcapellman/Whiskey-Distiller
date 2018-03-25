6b8139452cfbc097c929ebf13a33aba7d8ee950b
Jarred Capellman <jcapellman@hotmail.com>
2018-03-24 21:01:50 -0500

    Change: Initial work on the DBManager

--- src/WhiskeyDistiller.library/Managers/DBManager.cs
+++ src/WhiskeyDistiller.library/Managers/DBManager.cs
@@ -1,6 +1,20 @@
-﻿namespace WhiskeyDistiller.library.Managers
+﻿using SQLite;
+
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+
+namespace WhiskeyDistiller.library.Managers
 {
     public class DBManager : BaseManager
     {
+        public bool Add<T>(T obj) where T : BaseTable
+        {
+            using (var sqlConnection = new SQLiteConnection(Constants.DB_FILENAME))
+            {
+                sqlConnection.Insert(obj);
+                
+                return true;
+            }
+        }
     }
 }
