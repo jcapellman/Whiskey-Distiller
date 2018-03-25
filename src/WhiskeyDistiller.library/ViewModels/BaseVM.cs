4f73d1cdd585c7612362e947ff4d5263d0613d4e
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 13:48:49 -0500

    Added: Navigation Helper Wrapper and GoBackCommand to the BaseVM class

--- src/WhiskeyDistiller.library/ViewModels/BaseVM.cs
+++ src/WhiskeyDistiller.library/ViewModels/BaseVM.cs
@@ -1,9 +1,25 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
 
 namespace WhiskeyDistiller.library.ViewModels
 {
     public class BaseVM : INotifyPropertyChanged
     {
+        protected INavigation _navigation;
+
+        public BaseVM(INavigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        public async Task NavigateToAsync<T>() where T : ContentPage => await _navigation.PushModalAsync((T)Activator.CreateInstance(typeof(T)));
+
+        public ICommand GoBackCommand => new Command(async () => { await _navigation.PopModalAsync(); });
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
