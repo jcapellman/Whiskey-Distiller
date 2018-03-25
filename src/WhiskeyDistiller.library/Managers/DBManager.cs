7f81b94147943526d369e5891b340b66f31b1610
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 11:16:06 -0500

    Change: Removed the SQLite specific code from the DBManager in favor of the abstract interface reference

--- src/WhiskeyDistiller.library/Managers/DBManager.cs
+++ src/WhiskeyDistiller.library/Managers/DBManager.cs
@@ -1,23 +1,17 @@
-﻿using SQLite;
-
-using WhiskeyDistiller.library.Common;
-using WhiskeyDistiller.library.DAL.Tables;
-using WhiskeyDistiller.library.Implementations;
+﻿using WhiskeyDistiller.library.DAL.Tables;
+using WhiskeyDistiller.library.Interfaces;
 
 namespace WhiskeyDistiller.library.Managers
 {
     public class DBManager : BaseManager
     {
-        public DBManager(SQLiteDatabase database) : base(database) { }
+        private IDatabase _database;
 
-        public bool Add<T>(T obj) where T : BaseTable
+        public DBManager(IDatabase database) { _database = database; }
+
+        public void Add<T>(T obj) where T : BaseTable
         {
-            using (var sqlConnection = new SQLiteConnection(Constants.DB_FILENAME))
-            {
-                sqlConnection.Insert(obj);
-                
-                return true;
-            }
+            _database.Add(obj);
         }
     }
 }
