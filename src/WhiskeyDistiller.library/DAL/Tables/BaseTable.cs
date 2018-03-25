5604a43da4d2ff46d49208a32988e356f90fd70c
Jarred Capellman <jcapellman@hotmail.com>
2018-03-24 21:03:27 -0500

    Added: Initial work on the BaseTable Class

---
+++ src/WhiskeyDistiller.library/DAL/Tables/BaseTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WhiskeyDistiller.library.DAL.Tables
+{
+    public class BaseTable
+    {
+        public int ID { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Modified { get; set; }
+    }
+}
