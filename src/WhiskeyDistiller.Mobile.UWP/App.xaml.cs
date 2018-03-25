85c8b7d4c171f1d6293739506c9e068530f1302f
Jarred Capellman <jcapellman@hotmail.com>
2018-03-25 11:11:29 -0500

    Added: IoC Initialization in the App Class

--- src/WhiskeyDistiller.Mobile.UWP/App.xaml.cs
+++ src/WhiskeyDistiller.Mobile.UWP/App.xaml.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using WhiskeyDistiller.library.Common;
+
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-using Ninject;
+using WhiskeyDistiller.library.Common;
 
 namespace WhiskeyDistiller.Mobile.UWP
 {
@@ -33,7 +23,7 @@ namespace WhiskeyDistiller.Mobile.UWP
         {
             this.InitializeComponent();
 
-            IKernel kernel = new StandardKernel(new IocModule());
+            IoC.Setup();
 
             this.Suspending += OnSuspending;
         }
