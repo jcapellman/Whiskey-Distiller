46f006ea1b70d70b4e1b27b0b3a0704dbcda2eb3
Jarred Capellman <jcapellman@hotmail.com>
2018-03-26 19:56:01 -0500

    Added: Initial work on the LoadGamePage class

---
+++ src/WhiskeyDistiller.library/Views/LoadGamePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoadGamePage : ContentPage
+	{
+		public LoadGamePage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new LoadGamePageVM(Navigation);
+		}
+	}
+}
