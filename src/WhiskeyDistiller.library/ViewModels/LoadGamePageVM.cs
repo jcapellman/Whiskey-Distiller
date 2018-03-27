87b87d95744081cef16b13d54265b2e5a7dc80fa
Jarred Capellman <jcapellman@hotmail.com>
2018-03-26 19:54:17 -0500

    Added: Initial work on the LoadGamePageVM class

---
+++ src/WhiskeyDistiller.library/ViewModels/LoadGamePageVM.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class LoadGamePageVM : BaseVM
+    {
+        public LoadGamePageVM(INavigation navigation) : base(navigation)
+        {
+        }
+    }
+}
