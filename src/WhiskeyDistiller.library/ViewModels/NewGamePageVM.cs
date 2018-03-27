dc6021f42702e3729bafa226b6fb121dd65766ea
Jarred Capellman <jcapellman@hotmail.com>
2018-03-26 19:51:47 -0500

    Change: Renamed NewGameVM to NewGamePageVM

---
+++ src/WhiskeyDistiller.library/ViewModels/NewGamePageVM.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class NewGamePageVM : BaseVM
+    {
+        public NewGamePageVM(INavigation navigation) : base(navigation) { }
+    }
+}
--- src/WhiskeyDistiller.library/ViewModels/NewGameVM.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xamarin.Forms;
-
-namespace WhiskeyDistiller.library.ViewModels
-{
-    public class NewGameVM : BaseVM
-    {
-        public NewGameVM(INavigation navigation) : base(navigation) { }
-    }
-}
