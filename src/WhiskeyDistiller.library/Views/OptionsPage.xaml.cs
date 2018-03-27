e7460bae5d9336a75c50ca25a4b952cf55be1d49
Jarred Capellman <jcapellman@hotmail.com>
2018-03-26 19:56:20 -0500

    Added: Initial work on the OptionsPage class

---
+++ src/WhiskeyDistiller.library/Views/OptionsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OptionsPage : ContentPage
+	{
+		public OptionsPage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new OptionsPageVM(Navigation);
+		}
+	}
+}
