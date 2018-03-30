5a4ce60a5c41802b06d1ee9724a743e4029eeb68
Jarred Capellman <jcapellman@hotmail.com>
2018-03-29 21:46:57 -0500

    Added: Blank MainGamePage class

---
+++ src/WhiskeyDistiller.library/Views/MainGamePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainGamePage : ContentPage
+	{
+		public MainGamePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
