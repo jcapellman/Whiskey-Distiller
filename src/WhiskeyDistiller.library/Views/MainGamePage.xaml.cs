2afde5ab033078c49036a1e52838f43dda65e1cf
Jarred Capellman <jcapellman@hotmail.com>
2018-03-31 08:46:58 -0500

    Added: Initial work on the Options Popup in the MainGamePage

--- src/WhiskeyDistiller.library/Views/MainGamePage.xaml.cs
+++ src/WhiskeyDistiller.library/Views/MainGamePage.xaml.cs
@@ -5,7 +5,6 @@ using Xamarin.Forms.Xaml;
 
 namespace WhiskeyDistiller.library.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainGamePage : ContentPage
 	{
         private MainGamePageVM viewModel => (MainGamePageVM)BindingContext;
