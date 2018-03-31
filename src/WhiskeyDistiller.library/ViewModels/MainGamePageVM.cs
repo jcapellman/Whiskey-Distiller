bf7fb319da37eac8a494e2ea51ccc2d3fa9be708
Jarred Capellman <jcapellman@hotmail.com>
2018-03-30 21:55:05 -0500

    Added: Initial work on the MainGamePageVM

---
+++ src/WhiskeyDistiller.library/ViewModels/MainGamePageVM.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+using WhiskeyDistiller.library.Views;
+
+using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class MainGamePageVM : BaseVM
+    {
+        public MainGamePageVM(INavigation navigation) : base(navigation)
+        {
+        }
+
+        public ICommand OptionsCommand => NavigateCommand<MainPage>();
+        
+        public ICommand NextTurnCommand => NavigateCommand<MainPage>();
+    }
+}
