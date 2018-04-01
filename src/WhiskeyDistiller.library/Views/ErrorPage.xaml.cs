e33fdbe1c036a0967fb8784ba702ee8938e89e50
Jarred Capellman <jcapellman@hotmail.com>
2018-04-01 09:24:13 -0500

    Added: Initial work on the ErrorPage

---
+++ src/WhiskeyDistiller.library/Views/ErrorPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ErrorPage : ContentPage
+	{
+		public ErrorPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
