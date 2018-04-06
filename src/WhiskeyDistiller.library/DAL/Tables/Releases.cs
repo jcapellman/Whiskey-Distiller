a6e708f7df849f5c64b738ce11cb7a061cae7832
Jarred Capellman <jcapellman@hotmail.com>
2018-04-05 19:44:27 -0500

    Added: Initial work on the Releases table

---
+++ src/WhiskeyDistiller.library/DAL/Tables/Releases.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhiskeyDistiller.library.DAL.Tables
+{
+    public class Releases : BaseTable
+    {
+        [Required]
+        [ForeignKey("GameID")]
+        public Game Game { get; set; }
+
+        public int GameID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Inventory { get; set; }
+
+        [Required]
+        public int Revenue { get; set; }
+    }
+}
