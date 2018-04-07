a5f8c5947c1fec6b1a31cfcdd35ae546066a71e8
Jarred Capellman <jcapellman@hotmail.com>
2018-04-06 22:01:55 -0500

    Added: WarehouseTypes Enum

---
+++ src/WhiskeyDistiller.library/Enums/WarehouseTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace WhiskeyDistiller.library.Enums
+{
+    public enum WarehouseTypes
+    {
+        SMALLEST = 50,
+        SMALL = 100,
+        MEDIUM = 1000,
+        LARGE = 5000,
+        LARGEST = 10000
+    }
+}
