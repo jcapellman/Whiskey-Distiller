bf1d8015fef242dbc5aaf794e9f6f67c21cb3ff9
Jarred Capellman <jcapellman@hotmail.com>
2018-04-06 21:48:18 -0500

    Added: Initial commit of the Batch Table

---
+++ src/WhiskeyDistiller.library/DAL/Tables/Batch.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using WhiskeyDistiller.library.Enums;
+
+namespace WhiskeyDistiller.library.DAL.Tables
+{
+    public class Batch : BaseTable
+    {
+        [Required]
+        [ForeignKey("WarehouseID")]
+        public Warehouse Warehouse { get; set; }
+
+        public int WarehouseID { get; set; }
+
+        public string Name { get; set; }
+        
+        [Required]
+        public int NumberOfBarrels { get; set; }
+
+        [Required]
+        public int MonthsAged { get; set; }
+
+        [Required]
+        public double Proof { get; set; }
+
+        [Required]
+        public WhiskeyTypes Type { get; set; }
+
+        [Required]
+        public int Quality { get; set; }
+    }
+}
