af973bf462a7dc24ae5e81be4076ffa7887d6e9e
Jarred Capellman <jcapellman@hotmail.com>
2018-04-06 21:49:59 -0500

    Added: Initial work on the Warehouse Table

---
+++ src/WhiskeyDistiller.library/DAL/Tables/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhiskeyDistiller.library.DAL.Tables
+{
+    public class Warehouse : BaseTable
+    {
+        [Required]
+        [ForeignKey("GameID")]
+        public Game Game { get; set; }
+
+        public int GameID { get; set; }
+
+        public string Name { get; set; }
+
+        public int BarrelCapacity { get; set; }
+    }
+}
