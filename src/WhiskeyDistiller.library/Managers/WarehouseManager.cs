396e91c686e6f25a41d094d79e78480c55aa88e7
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 15:12:27 -0500

    Added: Initial work on the WarehouseManager

---
+++ src/WhiskeyDistiller.library/Managers/WarehouseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+using WhiskeyDistiller.library.Enums;
+
+namespace WhiskeyDistiller.library.Managers
+{
+    public class WarehouseManager : BaseManager
+    { 
+        public void AddWarehouse(int GameID, string name, WarehouseTypes warehouseType)
+        {
+            var warehouse = new Warehouse
+            {
+                GameID = GameID,
+                Name = name,
+                WarehouseType = warehouseType
+            };
+
+            IoC.DatabaseManager.Add(warehouse);
+        }
+        
+        public List<Warehouse> GetWarehouses(int GameID)
+        {
+            return IoC.DatabaseManager.Select<Warehouse>(a => a.GameID == GameID && a.Active);
+        }
+    }
+}
