bdc8f2253bc437bdfdaa0e96b27ee5bbe5db71d5
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 15:12:48 -0500

    Added: Initial work on the WarehousePage

---
+++ src/WhiskeyDistiller.library/Views/WarehousePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WarehousePage : ContentPage
+	{
+		public WarehousePage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new WarehousePageVM(Navigation);
+		}
+	}
+}
