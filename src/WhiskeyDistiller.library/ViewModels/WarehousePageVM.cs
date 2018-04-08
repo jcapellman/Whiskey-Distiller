1b422a8294bd840a9771570d036d601e6d37c1b0
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 15:13:06 -0500

    Added: Initial work on the WarehousePageVM

---
+++ src/WhiskeyDistiller.library/ViewModels/WarehousePageVM.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class WarehousePageVM : BaseVM
+    {
+        public WarehousePageVM(INavigation navigation) : base(navigation)
+        {
+        }
+    }
+}
