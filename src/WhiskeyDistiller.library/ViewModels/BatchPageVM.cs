ff874c9acc05ecee10de7909c6e62ff1392d9f2b
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 16:17:08 -0500

    Added: Initial work on the BatchPageVM

---
+++ src/WhiskeyDistiller.library/ViewModels/BatchPageVM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using WhiskeyDistiller.library.DAL.Tables;
+
+using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class BatchPageVM : BaseVM
+    {
+        private List<Release> _currentReleases;
+
+        public List<Release> CurrentReleases
+        {
+            get { return _currentReleases; }
+            set { _currentReleases = value; OnPropertyChanged("CurrentReleases"); }
+        }
+
+        public BatchPageVM(INavigation navigation) : base(navigation)
+        {
+        }
+    }
+}
