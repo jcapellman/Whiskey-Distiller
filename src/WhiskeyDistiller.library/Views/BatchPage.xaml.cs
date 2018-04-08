2bca8b8814d81ed8fa0d1a1a54f97809fb7c8618
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 16:15:17 -0500

    Added: Initial work on the BatchPage

---
+++ src/WhiskeyDistiller.library/Views/BatchPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BatchPage : ContentPage
+	{
+		public BatchPage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new BatchPageVM(Navigation);
+		}
+	}
+}
