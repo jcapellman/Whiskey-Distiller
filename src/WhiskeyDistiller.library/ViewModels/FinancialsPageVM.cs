9d30d31864670c7cc8e23afd603008f3be410dbc
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 20:00:58 -0500

    Added: Initial work on the FinancialsPageVM

---
+++ src/WhiskeyDistiller.library/ViewModels/FinancialsPageVM.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class FinancialsPageVM : BaseVM
+    {
+        public FinancialsPageVM(INavigation navigation) : base(navigation)
+        {
+        }
+    }
+}
