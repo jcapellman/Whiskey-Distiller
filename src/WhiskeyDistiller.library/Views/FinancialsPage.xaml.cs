4eb910b6f75c80a0652a6b933d6716caad0b671e
Jarred Capellman <jcapellman@hotmail.com>
2018-04-08 19:48:27 -0500

    Added: Initial commit of the FinancialsPage

---
+++ src/WhiskeyDistiller.library/Views/FinancialsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FinancialsPage : ContentPage
+	{
+		public FinancialsPage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new FinancialsPageVM(Navigation);
+		}
+	}
+}
