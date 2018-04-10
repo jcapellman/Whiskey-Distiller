80cf5b370868165546ba853880103cd2c2e9ed2a
Jarred Capellman <jcapellman@hotmail.com>
2018-04-09 20:02:28 -0500

    Added: Initial work on the EventsPage

---
+++ src/WhiskeyDistiller.library/Views/EventsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EventsPage : ContentPage
+	{
+		public EventsPage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new EventsPageVM(Navigation);
+		}
+	}
+}
