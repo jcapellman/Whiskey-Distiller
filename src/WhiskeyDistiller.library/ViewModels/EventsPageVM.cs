1d97eb277109a56423f5b9cdbe8f081b29a6685e
Jarred Capellman <jcapellman@hotmail.com>
2018-04-09 20:02:01 -0500

    Added: Initial work on the EventsPageVM class

---
+++ src/WhiskeyDistiller.library/ViewModels/EventsPageVM.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace WhiskeyDistiller.library.ViewModels
+{
+    public class EventsPageVM : BaseVM
+    {
+        public EventsPageVM(INavigation navigation) : base(navigation)
+        {
+        }
+    }
+}
