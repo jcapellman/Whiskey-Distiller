54d1fe12b59f39eb3ab25e59fb0476e73344f9f0
Jarred Capellman <jcapellman@hotmail.com>
2018-04-09 21:25:12 -0500

    Added: Initial work on the Event Table

---
+++ src/WhiskeyDistiller.library/DAL/Tables/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace WhiskeyDistiller.library.DAL.Tables
+{
+    public class Event : BaseTable
+    {
+        public string Date { get; set; }
+
+        public string Description { get; set; }
+
+        public int GameID { get; set; }
+
+        public bool Read { get; set; }
+    }
+}
