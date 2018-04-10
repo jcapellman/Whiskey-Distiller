4032fa581a692a1edec923d8e7706b9c627016f0
Jarred Capellman <jcapellman@hotmail.com>
2018-04-09 21:25:33 -0500

    Added: Initial work on the EventManager class

---
+++ src/WhiskeyDistiller.library/Managers/EventManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+
+namespace WhiskeyDistiller.library.Managers
+{
+    public class EventManager : BaseManager
+    {
+        public void AddEvent(string description, int gameID, string date)
+        {
+            var eventRow = new Event
+            {
+                Date = date,
+                Description = description,
+                GameID = gameID,
+                Read = false
+            };
+
+            IoC.DatabaseManager.Add(eventRow);
+        }
+
+        public List<Event> GetEvents(int gameID)
+        {
+            var events = IoC.DatabaseManager.Select<Event>(a => a.GameID == gameID && a.Active && !a.Read).OrderByDescending(a => a.Date).ToList();
+
+            return events;
+        }
+    }
+}
