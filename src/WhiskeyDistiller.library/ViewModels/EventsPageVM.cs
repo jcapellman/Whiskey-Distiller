f8d40b4e23280e55705d2aeb18f3f1e3564830ad
Jarred Capellman <jcapellman@hotmail.com>
2018-04-09 21:24:15 -0500

    Added: Events MVVM Binding in the EventsPageVM

--- src/WhiskeyDistiller.library/ViewModels/EventsPageVM.cs
+++ src/WhiskeyDistiller.library/ViewModels/EventsPageVM.cs
@@ -1,11 +1,24 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+
+using Xamarin.Forms;
 
 namespace WhiskeyDistiller.library.ViewModels
 {
     public class EventsPageVM : BaseVM
     {
+        private List<Event> _events;
+
+        public List<Event> Events
+        {
+            get { return _events; }
+            set { _events = value; OnPropertyChanged("Events"); }
+        }
+
         public EventsPageVM(INavigation navigation) : base(navigation)
         {
+            Events = IoC.EventManager.GetEvents(IoC.GameManager.CurrentGame.ID);
         }
     }
 }
