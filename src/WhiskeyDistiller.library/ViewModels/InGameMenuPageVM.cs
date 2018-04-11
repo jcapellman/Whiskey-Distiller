f9cfacc01b878e739e9f0e7d53fece72d073ef24
Jarred Capellman <jcapellman@hotmail.com>
2018-04-10 20:16:34 -0500

    Added: GotoSaveGameCommand to the InGameMenuPageVM

--- src/WhiskeyDistiller.library/ViewModels/InGameMenuPageVM.cs
+++ src/WhiskeyDistiller.library/ViewModels/InGameMenuPageVM.cs
@@ -1,9 +1,15 @@
-﻿using Xamarin.Forms;
+﻿using System.Windows.Input;
+
+using Xamarin.Forms;
+
+using WhiskeyDistiller.library.Views;
 
 namespace WhiskeyDistiller.library.ViewModels
 {
     public class InGameMenuPageVM : BaseVM
     {
+        public ICommand GotoSaveGameCommand => NavigateCommand<SavePage>();
+
         public InGameMenuPageVM(INavigation navigation) : base(navigation)
         {
         }
