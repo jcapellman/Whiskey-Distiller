c2f4ce52790cc328c3104662be74ec6a8bda446d
Jarred Capellman <jcapellman@hotmail.com>
2018-04-10 20:18:04 -0500

    Added: Initial work on the SavePage

---
+++ src/WhiskeyDistiller.library/Views/SavePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiskeyDistiller.library.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhiskeyDistiller.library.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SavePage : ContentPage
+	{
+		public SavePage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new SavePageVM(Navigation);
+		}
+	}
+}
