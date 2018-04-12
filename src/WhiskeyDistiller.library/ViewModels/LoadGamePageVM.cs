286796d6c564f8558d60c375dab2ba2c6b5c9edc
Jarred Capellman <jcapellman@hotmail.com>
2018-04-11 22:09:49 -0500

    Added: Initial work on the LoadGamePageVM

--- src/WhiskeyDistiller.library/ViewModels/LoadGamePageVM.cs
+++ src/WhiskeyDistiller.library/ViewModels/LoadGamePageVM.cs
@@ -1,11 +1,25 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+
+using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.DAL.Tables;
+
+using Xamarin.Forms;
 
 namespace WhiskeyDistiller.library.ViewModels
 {
     public class LoadGamePageVM : BaseVM
     {
+        private List<Game> _games;
+
+        public List<Game> Games
+        {
+            get { return _games; }
+            set { _games = value; OnPropertyChanged("Games"); }
+        }
+
         public LoadGamePageVM(INavigation navigation) : base(navigation)
         {
+            Games = IoC.GameManager.GetSavedGames();
         }
     }
 }
