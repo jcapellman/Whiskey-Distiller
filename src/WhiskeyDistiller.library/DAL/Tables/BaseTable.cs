f2b0b3c2b305a5bf36e81bb569fe66b209ed89ef
Jarred Capellman <jcapellman@hotmail.com>
2018-10-20 08:02:29 -0400

    Change: Removed the Active property in the BaseTable class

--- src/WhiskeyDistiller.library/DAL/Tables/BaseTable.cs
+++ src/WhiskeyDistiller.library/DAL/Tables/BaseTable.cs
@@ -5,9 +5,7 @@ namespace WhiskeyDistiller.library.DAL.Tables
     public class BaseTable
     {
         public int ID { get; set; }
-
-        public bool Active { get; set; }
-
+        
         public DateTime Created { get; set; }
 
         public DateTime Modified { get; set; }
