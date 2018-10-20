d60e9830b616c972a74a41d09b239a0ed90d3e62
Jarred Capellman <jcapellman@hotmail.com>
2018-10-19 22:07:15 -0400

    Added: LiteDBDatabase implementation

--- src/WhiskeyDistiller.library/Implementations/LiteDBDatabase.cs
+++ src/WhiskeyDistiller.library/Implementations/LiteDBDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 using WhiskeyDistiller.library.DAL.Tables;
@@ -9,14 +10,21 @@ namespace WhiskeyDistiller.library.Implementations
 {
     public class LiteDBDatabase : IDatabase
     {
+        private const string DB_FILE_NAME = "game.db";
+
         public bool InitializeDB()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public bool Add<T>(T obj) where T : BaseTable
         {
-            throw new NotImplementedException();
+            using (var db = new LiteDB.LiteDatabase(DB_FILE_NAME))
+            {
+                var collection = db.GetCollection<T>();
+
+                return collection.Insert(obj);
+            }
         }
 
         public bool CreateTable(Type tableType)
@@ -26,17 +34,35 @@ namespace WhiskeyDistiller.library.Implementations
 
         public List<T> Select<T>(Expression<Func<T, bool>> expression) where T : new()
         {
-            throw new NotImplementedException();
+            using (var db = new LiteDB.LiteDatabase(DB_FILE_NAME))
+            {
+                var collection = db.GetCollection<T>();
+
+                return collection.Find(expression).ToList();
+            }
         }
 
         public void Remove<T>(T obj) where T : BaseTable
         {
-            throw new NotImplementedException();
+            using (var db = new LiteDB.LiteDatabase(DB_FILE_NAME))
+            {
+                if (obj == null)
+                {
+                    return;
+                }
+
+                var collection = db.GetCollection<T>();
+
+                collection.Delete(obj.ID);
+            }
         }
 
         public void Update<T>(T obj) where T : BaseTable
         {
-            throw new NotImplementedException();
+            using (var db = new LiteDB.LiteDatabase(DB_FILE_NAME))
+            {
+                db.GetCollection<T>().Update(obj);
+            }
         }
     }
 }
