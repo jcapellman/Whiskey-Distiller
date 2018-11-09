bcbd0f5ac8d98e57acd9572de25820d358d63181
Jarred Capellman <jcapellman@hotmail.com>
2018-11-08 21:28:55 -0600

    Added: WarehousePageVM Add implementation

--- src/WhiskeyDistiller.library/ViewModels/WarehousePageVM.cs
+++ src/WhiskeyDistiller.library/ViewModels/WarehousePageVM.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using WhiskeyDistiller.library.Common;
+using WhiskeyDistiller.library.Enums;
+
+using Xamarin.Forms;
 
 namespace WhiskeyDistiller.library.ViewModels
 {
@@ -6,6 +9,27 @@ namespace WhiskeyDistiller.library.ViewModels
     {
         public WarehousePageVM(INavigation navigation) : base(navigation)
         {
+        }
+
+        private string _gameName;
+
+        public string GameName
+        {
+            get => _gameName;
+            set { _gameName = value; OnPropertyChanged(nameof(GameName)); }
+        }
+
+        private WarehouseTypes _warehouseType;
+
+        public WarehouseTypes WarehouseType
+        {
+            get => _warehouseType;
+            set { _warehouseType = value; OnPropertyChanged(nameof(WarehouseType)); }
+        }
+
+        public void AddWarehouse()
+        {
+            IoC.WarehouseManager.AddWarehouse(IoC.GameManager.CurrentGame.ID, GameName, WarehouseType);
         }
     }
 }
