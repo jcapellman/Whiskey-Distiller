54856ed68445843da06234837de1264ab8711729
Jarred Capellman <jcapellman@hotmail.com>
2018-11-15 22:26:08 -0600

    Added: ReturnSet Wrapper

---
+++ src/WhiskeyDistiller.library/Common/ReturnSet.cs
@@ -0,0 +1,14 @@
+﻿namespace WhiskeyDistiller.library.Common
+{
+    public class ReturnSet<T>
+    {
+        public T _value;
+
+        public T GetObject() => _value;
+
+        public ReturnSet(T objvalue)
+        {
+            _value = objvalue;
+        }
+    }
+}
