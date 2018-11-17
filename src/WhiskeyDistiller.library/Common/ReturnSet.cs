cd294fb9a48b4913606241d35277588af20bf772
Jarred Capellman <jcapellman@hotmail.com>
2018-11-16 21:20:38 -0600

    Added: Error and Object handling in the ReturnSet class

--- src/WhiskeyDistiller.library/Common/ReturnSet.cs
+++ src/WhiskeyDistiller.library/Common/ReturnSet.cs
@@ -1,14 +1,32 @@
-﻿namespace WhiskeyDistiller.library.Common
+﻿using System;
+
+namespace WhiskeyDistiller.library.Common
 {
     public class ReturnSet<T>
     {
-        public T _value;
+        public T Value;
 
-        public T GetObject() => _value;
+        public T Object => Value;
+
+        private readonly Exception _error;
+
+        public bool HasError => _error != null;
+
+        public Exception Error => _error;
+
+        private readonly string _additionalErrorInformation;
+
+        public string AdditionalErrorInformation => _additionalErrorInformation;
 
         public ReturnSet(T objvalue)
         {
-            _value = objvalue;
+            Value = objvalue;
+        }
+
+        public ReturnSet(Exception exception, string additionalErrorInformation = null)
+        {
+            _error = exception;
+            _additionalErrorInformation = additionalErrorInformation;
         }
     }
 }
