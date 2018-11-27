ce4aa13139e046daeae5266c1057e26dd1501f35
Jarred Capellman <jcapellman@hotmail.com>
2018-11-26 22:20:56 -0600

    Added: Add Warehouse button to the WarehousePage.xaml

--- src/WhiskeyDistiller.Mobile.Android/MainActivity.cs
+++ src/WhiskeyDistiller.Mobile.Android/MainActivity.cs
@@ -16,7 +16,7 @@ namespace WhiskeyDistiller.Mobile.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
